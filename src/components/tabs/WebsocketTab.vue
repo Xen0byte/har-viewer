89ab79c25c53e4445b716ea569eb8a648075bee3
Erik Bender <erik.bender@develerik.dev>
2021-08-21 00:18:30 +0200

    refactor websocket tab

--- src/components/tabs/WebsocketTab.vue
+++ src/components/tabs/WebsocketTab.vue
@@ -1,18 +1,37 @@
-<script>
-  export default {
-    name: "WebsocketTab",
-    props: {
-      data: {
-        type: Object,
-        required: true,
-      },
+<script setup>
+  import { computed } from "vue";
+
+  const props = defineProps({
+    data: {
+      type: Object,
+      required: true,
     },
-  };
+  });
+
+  // eslint-disable-next-line no-underscore-dangle
+  const receive = computed(() => props.data._webSocketMessages.filter(o => o.type === "receive"));
+  // TODO: implement send
 </script>
 
 <template>
-  <article
-    class="overflow-text"
-    v-text="data._webSocketMessages"
-  />
+  <div
+    v-if="props.data._webSocketMessages"
+    class="tab-content overflow-text"
+  >
+    <section>
+      <h1>Receive</h1>
+      <ul>
+        <li
+          v-for="(m, i) in receive"
+          :key="i"
+        >
+          <b>Time: {{ m.time }}</b>
+          <ul>
+            <li>OP Code: {{ m.opcode }}</li>
+            <li>Data: <code v-text="m.data" /></li>
+          </ul>
+        </li>
+      </ul>
+    </section>
+  </div>
 </template>
